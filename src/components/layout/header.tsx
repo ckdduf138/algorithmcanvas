b930da140f2fb989ef4e69fe01443a188ef35808
Chang Yeol <ckdduf138@naver.com>
2024-05-21 22:49:54 +0900

    feat: 테마 기본 기능 추가

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,41 +1,53 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useTheme } from '../../context/themeContext';
+import ToggleSwitch from '../common/toggleSwitch';
 
-const StyledHeader = styled.header`
+const StyledHeader = styled.header<{ theme: string }>`
     display: flex;
     justify-content: center;
-    flex-direction: column;
-    background-color: #15202b;
+    align-items: center;
+    background-color: ${({ theme: themeType }) => (themeType === 'light' ? '#ffffff' : '#15202b')};
     text-align: center;
     text-transform: uppercase;
     font-family: 'Raleway', cursive;
     position: relative;
-    width:100%;
-    height:10%;
+    width: 100%;
+    height: 10%;
+    color: ${({ theme: themeType }) => (themeType === 'light' ? '#000' : '#fff')};
 `;
 
 const HeadTitle = styled.div`
-    display: block;
     padding: 20px;
     text-decoration: none;
     letter-spacing: 30px;
-    color: white;
     cursor: pointer;
     font-size: 30px;
 `;
 
-const Header: React.FC = () => {
+const ToggleContainer = styled.div`
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    display: flex;
+    align-items: center;
+`;
 
+const Header: React.FC = () => {
     const navigate = useNavigate();
+    const { theme, toggleTheme } = useTheme();
 
     const header_onClicked = () => {
         navigate('/');
     };
 
     return (
-        <StyledHeader>
+        <StyledHeader theme={theme}>
             <HeadTitle onClick={header_onClicked}>Algo-Canvas</HeadTitle>
+            <ToggleContainer>
+                <ToggleSwitch onToggle={toggleTheme} />
+            </ToggleContainer>
         </StyledHeader>
     );
 };
