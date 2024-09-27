d31ae1e8cbe331e3e321ad6b4eac266de1f8f50c
Chang Yeol <ckdduf138@naver.com>
2024-09-27 16:59:06 +0900

    feat: dfs 추가

--- src/pages/graph/page.dfs.tsx
+++ src/pages/graph/page.dfs.tsx
@@ -1,11 +1,139 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Layout from '../../components/layout/layout';
 import GraphCanvas from '../../components/graphCanvas/graphCanvas';
+import { useGraphCanvas } from '../../hooks/graph/useGraphCanvas';
+import { EdgeFocusStatus, NodeFocusStatus } from '../../utils/graphData';
 
-const DFSPage: React.FC = () => (
-  <Layout subTitle='깊이 우선 탐색(DFS)'>
-    깊이 우선 탐색(DFS)
-  </Layout>
-);
+const DFSPage: React.FC = () => {
+  const isRunning = useRef(false);
+  const delayRef = useRef(500);
+
+  const { 
+    nodeGraphData, setNodeGraphData, setSeletedNode, nodeGraphDatas, draggingCircle, selectedEdge, selectedNode,  draggingEdge, CustomNode, CustomLink, 
+    handleMouseDown, handleEdgeClick, handleRandomizeGraphData, handleResetGraphData } 
+    = useGraphCanvas(isRunning, delayRef);
+
+  const updateNodeFocus = async (nodeId: string, updateState: NodeFocusStatus, nodeDepth?: number) => {
+    setNodeGraphData(prevData => {
+      if (!prevData) return prevData;
+  
+      const updatedNodes = prevData.nodes.map((node) => ({
+        ...node,
+        focus: node.id === nodeId ? updateState : node.focus,
+        text: nodeDepth !== undefined ? (node.id === nodeId ? nodeDepth?.toString() : node.text) : node.text,
+      }));
+  
+      return {
+        ...prevData,
+        nodes: updatedNodes,
+      };
+    });
+
+    await new Promise(resolve => setTimeout(resolve, delayRef.current));
+  }
+
+  const updateEdgeFocus = async (source: string, target: string, updateState: EdgeFocusStatus) => {
+    setNodeGraphData(prevData => {
+      if (!prevData) return prevData;
+      const updatedLinks = prevData.links.map((link) => {
+        if (source === link.target && target === link.source) {
+          return {
+            ...link,
+            source: link.target,
+            target: link.source,
+            focus: updateState
+          };
+        }
+    
+        if (source === link.source && target === link.target) {
+          return {
+            ...link,
+            focus: updateState
+          };
+        }
+
+        return link;
+      });
+    
+      return {
+        ...prevData,
+        links: updatedLinks,
+      };
+    });
+
+    await new Promise(resolve => setTimeout(resolve, delayRef.current));
+  }
+
+  const handleStart = async (startNodeId: string) => {
+    isRunning.current = true;
+  
+    const { nodes, links } = nodeGraphData;
+  
+    const visited = new Map<string, boolean>();
+    
+    const nodeDepthMap = new Map<string, number>();
+  
+    nodes.forEach((node) => visited.set(node.id, false));
+    
+    const dfs = async (currentNodeId: string, currentDepth: number) => {
+      visited.set(currentNodeId, true);
+      nodeDepthMap.set(currentNodeId, currentDepth);
+  
+      await updateNodeFocus(currentNodeId, 'active', currentDepth);
+  
+      const currentNode = nodes.find((node) => node.id === currentNodeId);
+      if (!currentNode) return;
+  
+      const neighbors = links
+        .filter((link) => link.source === currentNodeId || link.target === currentNodeId)
+        .map((link) => (link.source === currentNodeId ? link.target : link.source));
+  
+      for (const neighborId of neighbors) {
+        if (!visited.get(neighborId)) {
+          await updateEdgeFocus(currentNodeId, neighborId, 'active');
+          updateEdgeFocus(currentNodeId, neighborId, 'completed');
+  
+          await dfs(neighborId, currentDepth + 1);
+  
+          await updateNodeFocus(neighborId, 'completed');
+        }
+      }
+    };
+
+    await dfs(startNodeId, 0);
+  
+    new Promise((resolve) => setTimeout(resolve, delayRef.current));
+  
+    nodes.forEach((node) => { 
+      if(visited.get(node.id)) {
+        updateNodeFocus(node.id, 'completed');
+      }
+    });
+  
+    isRunning.current = false;
+    setSeletedNode(null);
+  };
+    
+  return(
+    <Layout subTitle='너비 우선 탐색(BFS)'>
+      <GraphCanvas 
+        nodeGraphDatas={nodeGraphDatas}
+        draggingCircle={draggingCircle}
+        selectedNode={selectedNode}
+        selectedEdge={selectedEdge}
+        draggingEdge={draggingEdge}
+        CustomNode={CustomNode}
+        CustomLink={CustomLink}
+        delayRef={delayRef}
+        isRunning={isRunning}
+        handleMouseDown={handleMouseDown}
+        handleEdgeClick={handleEdgeClick}
+        handleRandomizeGraphData={handleRandomizeGraphData}
+        handleResetGraphData={handleResetGraphData}
+        handleStart={handleStart}
+      />
+    </Layout>
+  )
+};
 
 export default DFSPage;
