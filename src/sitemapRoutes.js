60f3660ebacfa20cfe1bbfe0650a76f053f1ffae
unknown <ckdduf138@naver.com>
2024-10-01 14:16:50 +0900

    feature: google seo 추가

---
+++ src/sitemapRoutes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+    <Route>
+        <Route path="/"  />
+        <Route path="/home" />
+        <Route path="/paperstorage" />
+        <Route path="/history" />
+        <Route path="/userprofile" />
+        <Route path="/paper" />
+        <Route path="/historypaper" />
+        <Route path="/historytrash"  />
+    </Route>
+);
