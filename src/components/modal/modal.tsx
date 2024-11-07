ee06c86971f76ee1aad76c798ee34497aa3ed27e
Chang Yeol <ckdduf138@naver.com>
2024-11-07 22:49:06 +0900

    style: 모달 문구 수정

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -15,7 +15,8 @@ const ModalOverlay = styled.div`
 `;
 
 const ModalContent = styled.div`
-    width: 77%;
+    width: 55%;
+    min-width: 400px;
     background: white;
     padding: 30px;
     border-radius: 10px;
@@ -60,21 +61,19 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
         <Heading>알고리즘 캔버스에 오신 것을 환영합니다!</Heading>
 
         <Content>
-            Algorithm Canvas는 알고리즘 시각화를 통해 복잡한 알고리즘과 자료구조를 쉽게 이해할 수 있는 학습 플랫폼입니다. 
-            그래프 탐색, 정렬, 트리와 같은 다양한 알고리즘을 실시간 시뮬레이션으로 직관적으로 경험할 수 있습니다.
+            알고리즘 시각화를 통해 쉽게 이해할 수 있는 학습 플랫폼입니다. 다양한 알고리즘을 실시간으로 경험할 수 있습니다.
         </Content>
 
         <Content>
-            주요 알고리즘의 동작 과정을 눈으로 확인하며 직접 실습할 수 있어, 문제 해결 능력을 효과적으로 향상시킬 수 있습니다. 
-            이 플랫폼은 사용자 친화적인 UI로 설계되어 누구나 쉽게 사용할 수 있습니다.
+            동작 과정을 직접 실행하여 문제 해결 능력을 향상시켜보세요! 사용 친화적으로 설계된 UI로 쉽게 사용하실 수 있습니다.
         </Content>
 
         <Content>
-            알고리즘 학습이 필요한 학생, 개발자 및 모든 학습자를 위해 만들어진 Algorithm Canvas와 함께 더 깊이 있는 학습을 경험해 보세요.
+            Algorithm Canvas와 함께 깊이 있는 학습을 경험해 봅시다!
         </Content>
 
         <Button onClick={onClose}>시작하기</Button>
-        
+
       </ModalContent>
     </ModalOverlay>
   );
