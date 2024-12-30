b9fc92ea8e3c5e46d23807899be23bf91f45c5c6
Chang Yeol <ckdduf138@naver.com>
2024-12-30 15:44:21 +0900

    feat: footer contact 추가 및 이메일 하이퍼링크 (#220)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,31 +2,48 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { useTheme } from '../../context/themeContext';
-import Text from '../common/text';
 
-const StyledFooter = styled.footer<{ theme: string }>`
-    background-color: ${({ theme: themeType }) => (themeType === 'light' ? '#f7f8f9' : '#0b131b')};
+const FooterContainer = styled.footer<{ theme: string }>`
     width: 100%;
     min-height: 10%;
+    text-align: center;
+    padding: 10px;
+    background-color: ${({ theme: themeType }) => (themeType === 'light' ? '#f7f8f9' : '#0b131b')};
 `;
 
-const FooterText = styled.div`
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
+const FooterText = styled.p<{ theme: string }>`
+    color: ${props => props.theme === 'light' ? '#8A8F95' : '#b8bcbf'};
+    margin: 0;
+    font-size: 16px;
+
+    &.title {
+        font-size: 24px;
+        font-weight: bold;
+    }
+`;
+
+const EmailLink = styled.a<{ theme: string }>`
+    color: ${props => props.theme === 'light' ? '#8A8F95' : '#b8bcbf'};
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 const Footer: React.FC = () => {
     const { theme } = useTheme();
 
     return (
-        <StyledFooter theme={theme}>
+        <FooterContainer theme={theme}>
+            <FooterText className="title">algorithm-canvas</FooterText>
+
             <FooterText>
-                <Text fontSize='20px'>algorithm-canvas</Text>
-                <Text>© 2024 - 2024</Text>
+                Contact: <EmailLink href="mailto:acanvas2024@gmail.com">acanvas2024@gmail.com</EmailLink>
             </FooterText>
-        </StyledFooter>
+
+            <FooterText>© 2024 - 2025</FooterText>
+        </FooterContainer>
     );
 };
 
